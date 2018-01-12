00363dc32200f04179afe58fd76b9007bedf40d2
zj <suke.shou@gmail.com>
2018-01-12 23:20:57 +0800

    Add query to dynamodb

--- src/dynamodb.js
+++ src/dynamodb.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const AWS = require('aws-sdk')
 const docClient = new AWS.DynamoDB.DocumentClient()
 
@@ -5,13 +7,7 @@ exports.put = (tableName, item) => {
   const params = { TableName: tableName, Item: item }
 
   return new Promise((resolve, reject) => {
-    docClient.put(params, (err, data) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(data)
-      }
-    })
+    docClient.put(params, (err, data) => { err ? reject(err) : resolve(data) })
   })
 }
 
@@ -19,12 +15,32 @@ exports.get = (tableName, key) => {
   const params = { TableName: tableName, Key: key }
 
   return new Promise((resolve, reject) => {
-    docClient.get(params, (err, data) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(data)
-      }
-    })
+    docClient.get(params, (err, data) => { err ? reject(err) : resolve(data) })
+  })
+}
+
+exports.query = (tableName, keys) => {
+  let keyClause = '',
+    keyNames = {},
+    keyVals = {},
+    delim = ''
+
+  keys.forEach(item => {
+    const keyOp = item.op || '='
+    keyNames['#' + item.key] = item.key
+    keyVals[':' + item.key] = item.val
+    keyClause += delim + '#' + item.key + keyOp + ':' + item.key
+    delim = ' and '
+  })
+
+  const params = {
+    TableName: tableName,
+    KeyConditionExpression: keyClause,
+    ExpressionAttributeNames: keyNames,
+    ExpressionAttributeValues: keyVals
+  }
+
+  return new Promise((resolve, reject) => {
+    docClient.query(params, (err, data) => { err ? reject(err) : resolve(data) })
   })
 }
