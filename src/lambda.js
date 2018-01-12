dab180df5e732f8e09ee6da140425dfad4d14558
zj <suke.shou@gmail.com>
2018-01-12 16:45:02 +0800

    Edit authorization to use validator pass in from context

--- src/lambda.js
+++ src/lambda.js
@@ -1,18 +1,13 @@
-const jwt = require('jsonwebtoken')
-
 exports.handler = handler => ctx => (event, context, callback) => {
   console.log('Request:', JSON.stringify(event, null, 2))
 
-  const config = ctx.config
-  const authType = config.app.authType || 'jwt'
-
-  if (authType === 'jwt') {
-    const result = decodeJsonWebToken(event.headers.token, config.app.tokenKey)
-    if (!result.ok) {
-      callback(new Error('jwt.invalid'))
-    } else {
-      event.vars = { token: result.claim }
+  if (ctx.authorize) {
+    const result = ctx.authorize(event)
+    if (result.err) {
+      callback(result.err)
+      return
     }
+    event.auth = result.data
   }
 
   const req = Object.assign({}, event, { body: event.body ? JSON.parse(event.body) : null })
@@ -28,13 +23,4 @@ exports.handler = handler => ctx => (event, context, callback) => {
   }
 
   handler(req, resp, ctx)
-}
-
-function decodeJsonWebToken(token, key) {
-  try {
-    const claim = jwt.verify(token, key)
-    return { claim, ok: true }
-  } catch (e) {
-    return { claim: null, ok: false }
-  }
 }
